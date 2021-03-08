4d01231ff5978d8a9fd0cde4e51b373e04987c7b
Nowfil <ap049.applepro@gmail.com>
2021-03-08 13:11:21 +0530

    gitignore changes.

---
+++ loki-bridge/src/config/config.js
@@ -0,0 +1,13 @@
+export default {
+    apiUrl: 'http://localhost:8000',
+    useAPIEncryption: false,
+    binance: {
+      walletCreationUrl: 'https://testnet.binance.org/en/create',
+      txExplorerUrl: 'https://testnet-explorer.binance.org/tx'
+    },
+    beldex: {
+      walletCreationUrl: 'https://wallet.beldex.io/#!/',
+      txExplorerUrl: 'https://explorer.beldex.io/'
+    }
+  };
+  
