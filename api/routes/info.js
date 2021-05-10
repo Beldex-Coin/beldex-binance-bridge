b23cc5a420d464fc7d3d5c45d7ee934168df1866
Your Name <ap049.applepro@gmail.com>
2021-05-10 14:10:29 +0530

    get balance.

--- api/routes/info.js
+++ api/routes/info.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import config from 'config';
 import clients from '../../processing/core/binance';
+import Web3 from 'web3';
 
 export function getInfo(req, res, next) {
   const beldexFee = config.get('beldex.withdrawalFee');
@@ -18,10 +19,15 @@ export function getInfo(req, res, next) {
 }
 
 export async function getBalance(req, res, next) {
+  const bscUrl = config.get('bsc.url');
+  const Web3js = await new Web3(await new Web3.providers.HttpProvider(bscUrl));
+  let balance = await new Web3js.eth.getBalance('0xdFc515De6EA5eE5502FF1Bdd5b5Bded2C41140Fb');
+  console.log("balance :",(balance/1e9)/1e9,balance)
   const binanceAddress = config.get('binance.depositAddress');
   const clien = clients.getClient();
-  const getBalance = await clien.getBalance(binanceAddress);
-  const totalBbdxSupply = config.get('binance.totalSupply');
+  const getBalance = await clien.getBalance('0xdFc515De6EA5eE5502FF1Bdd5b5Bded2C41140Fb');
+  console.log("et:",getBalance)
+  // const totalBbdxSupply = config.get('binance.totalSupply');
   let beldexBalance = getBalance.filter(data => data.symbol === config.get('binance.symbol'));
   beldexBalance[0].totalSupply = totalBbdxSupply;
   beldexBalance[0].movedBalance = totalBbdxSupply - Number(beldexBalance[0].free);
