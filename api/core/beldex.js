8cc256b831cc2f89dc722ea57972956112b26c29
lifwon <lifwon06@gmail.com>
2021-05-31 10:32:23 +0530

    walletrpc configuration changes.

--- api/core/beldex.js
+++ api/core/beldex.js
@@ -1,20 +1,33 @@
 import config from 'config';
 import { clients } from 'bridge-core';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const { filename, password: walletPassword, accountIndex } = config.get('beldex.wallet');
-const { host, port, username, password: rpcPassword } = config.get('beldex.walletRPC');
+// const { filename, password, accountIndex } = config.get('beldex.wallet');
+// const { host, port, username, password } = config.get('beldex.walletRPC');
 
+// const rpc = {
+//   hostname: process.env.BELDEX_WALLETRPC_HOST,
+//   port,
+//   username,
+//   password,
+// };
 const rpc = {
-  hostname: host,
-  port,
-  username,
-  password: rpcPassword,
+  hostname: process.env.BELDEX_WALLETRPC_HOST,
+  port: process.env.BELDEX_WALLETRPC_PORT,
+  username: process.env.BELDEX_WALLETRPC_USERNAME,
+  password: process.env.BELDEX_WALLETRPC_PASSWORD,
 };
 
+// const wallet = {
+//   filename,
+//   password,
+//   accountIndex,
+// };
 const wallet = {
-  filename,
-  password: walletPassword,
-  accountIndex,
+  filename: process.env.BELDEX_WALLET_FILENAME,
+  password: process.env.BELDEX_WALLET_PASSWORD,
+  accountIndex: Number(process.env.BELDEX_WALLET_ACCOUNTINDEX),
 };
 
 export default new clients.beldexClient(rpc, wallet);
