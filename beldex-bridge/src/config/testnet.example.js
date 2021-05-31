5d61096a42b95275a815ce716ff33c23afbc0ebb
lifwon <lifwon06@gmail.com>
2021-05-31 19:28:46 +0530

    fake chainId removed.

--- beldex-bridge/src/config/testnet.example.js
+++ beldex-bridge/src/config/testnet.example.js
@@ -3,10 +3,10 @@ export default {
   useAPIEncryption: false,
   binance: {
     walletCreationUrl: 'https://bscscan.com/address/0x90bbddbf3223363898065b9c736e2b86c655762b',
-    txExplorerUrl: 'https://testnet-explorer.binance.org/tx'
+    txExplorerUrl: 'https://bscscan.com/tx'
   },
   beldex: {
     walletCreationUrl: 'https://wallet.beldex.io/#!/',
-    txExplorerUrl: 'https://explorer.beldex.io/'
+    txExplorerUrl: 'https://explorer.beldex.io/tx'
   }
 };
